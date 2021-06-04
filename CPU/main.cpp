5819d0c727206f1a45ed6cb1219b447529bac88f
barnabasp <barnabas.porfy@gmail.com>
2021-06-04 17:50:57 +0200

    Initial commit

---
+++ CPU/main.cpp
@@ -0,0 +1,12 @@
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// - Author: Barnabas Porfy
+// - Game of Life: Parallel version
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//
+#include "funcs.hpp"
+#include <iostream>
+
+int main(int, char**) 
+{
+    std::cout << "Hello, world!\n";
+}
