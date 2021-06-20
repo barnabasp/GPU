20c650b5a603ae2317307d2bb8049e223ab5085c
barnabasp <barnabas.porfy@gmail.com>
2021-06-20 14:33:55 +0200

    Removed a line

--- GPU/ConwayGPU.cpp
+++ GPU/ConwayGPU.cpp
@@ -12,6 +12,9 @@
 //time
 #include <chrono>
 
+void writeGrid(std::vector<int> grid, unsigned int gen, time_t genTime, unsigned int N, unsigned int M);
+void dumpGrid(std::vector<int> grid, unsigned int gen, time_t genTime, unsigned int N, unsigned int M, bool print);
+
 int main()
 {
     try
@@ -40,52 +43,24 @@ int main()
         queue.finish();
         */
         //cl::Kernel kernel(program, "applyRules");
-        auto saxpy = cl::KernelFunctor<cl_double, cl::Buffer, cl::Buffer>(program, "applyRules");
+        auto conway = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl_int, cl_int, cl_int>(program, "applyRules");
 
         // Init computation
+        /*
         const std::size_t chainlength = std::size_t(std::pow(2u, 4u)); // 1M, cast denotes floating-to-integral conversion,
                                                                         //     promises no data is lost, silences compiler warning
         std::vector<cl_float> vec_x(chainlength),
                               vec_y(chainlength);
         cl_float a = 2.0;
-
+        */
         // Fill arrays with random values between 0 and 100
+        /*
         auto prng = [engine = std::default_random_engine{},
                      distribution = std::uniform_real_distribution<cl_float>{ -100.0, 100.0 }]() mutable { return distribution(engine); };
-
-        std::generate_n(std::begin(vec_x), chainlength, prng);
-        std::generate_n(std::begin(vec_y), chainlength, prng);
-
-        cl::Buffer buf_x{ std::begin(vec_x), std::end(vec_x), true },
-                   buf_y{ std::begin(vec_y), std::end(vec_y), false };
-
-        // Explicit (blocking) dispatch of data before launch
-        cl::copy(queue, std::begin(vec_x), std::end(vec_x), buf_x);
-        cl::copy(queue, std::begin(vec_y), std::end(vec_y), buf_y);
-
-        // Launch kernels
-        //queue.enqueueNDRangeKernel(saxpy, cl::NDRange{ chainlength }, a, buf_x, buf_y);
-        saxpy(cl::EnqueueArgs{ queue, cl::NDRange{ chainlength } }, a, buf_x, buf_y);
-
-
-        std::transform(
-            vec_x.cbegin(),
-            vec_x.cbegin(),
-            vec_y.cbegin(),
-            vec_x.begin(),
-            [=](const cl_float& x, const cl_float& y){ return a * x + y; }
-        );
-
-        // (Blocking) fetch of results
-        cl::copy(queue, buf_y, std::begin(vec_y), std::end(vec_y));
-
-        // Validate (compute saxpy on host and match results)
-        if (std::equal(vec_x.cbegin(), vec_x.cend(), vec_y.cbegin()))
-            throw std::runtime_error{ "Validation failed." };
-        cl::finish();
-        /*
-        size_t N = 6;
-        size_t M = 6;
+        */
+        unsigned int N = 1e3;
+        unsigned int M = 1e3;
+        unsigned int rule = 2;
         std::array<std::vector<cl_int>, 2> grids;
         unsigned int generation = 10;
 
@@ -94,13 +69,63 @@ int main()
         std::random_device rd;  //Will be used to obtain a seed for the random number engine
         std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
         std::uniform_real_distribution<> dis(0.0, 1.0);
-        for (int i = 0; i < (N * M); i++)
+        for (unsigned int i = 0; i < (N * M); i++)
         {
                 grids[0][i] = (dis(gen) > 0.5 ? 0 : 1);
                 //grids[0][i] = 0;
         }
+        //blinker oscillator
+        /*
+        grids[0][2 + M * 2] = 1;
+        grids[0][2 + M * 3] = 1;
+        grids[0][2 + M * 4] = 1;
         */
+        //glider spaceship:
+        /*
+        grids[0][2 + M*2] = 1;
+        grids[0][3 + M*3] = 1;
+        grids[0][1 + M*4] = 1;
+        grids[0][2 + M*4] = 1;
+        grids[0][3 + M*4] = 1;
+        */
+
+        //container for time measurement
+        //not necessarily needed a vector, but it's stored always
+        std::vector<time_t> timeMeasure;
+        
+        std::cout << "Executing game of life with rule: " << rule << " on a grid " << N << "x" << M << std::endl;
+        //loop over the generations
+        for(unsigned int gen = 0; gen < 20; gen++)
+        {
+            //create a buffer with the grids
+            cl::Buffer buf_x{ std::begin(grids[0]), std::end(grids[0]), true },
+                       buf_y{ std::begin(grids[1]), std::end(grids[1]), false };
+
+            // Explicit (blocking) dispatch of data before launch
+            cl::copy(queue, std::begin(grids[0]), std::end(grids[0]), buf_x);
+            cl::copy(queue, std::begin(grids[1]), std::end(grids[1]), buf_y);
+
+            // Launch time measurement
+            auto t0 = std::chrono::high_resolution_clock::now();
+            //launch the kernel with the given arguments
+            //the dimension is set to N,M and we are treating it as a 1D vector
+            conway(cl::EnqueueArgs{ queue, cl::NDRange{ N,M } }, buf_x, buf_y, N, M, rule);
+            auto t1 = std::chrono::high_resolution_clock::now();
+            timeMeasure.push_back(std::chrono::duration_cast<std::chrono::microseconds>(t1-t0).count());
+
+
+            // (Blocking) fetch of results
+            cl::copy(queue, buf_y, std::begin(grids[0]), std::end(grids[0])); //basically copy the buffer for the next status to the current status;
+            //grids[0] = grids[1]; 
+            dumpGrid(grids[0], gen, timeMeasure[gen], N, M, false);
+            std::cout << '\n';
+        }
+
+        cl::finish();
     }    
+    //+-+-+-+-+-+-+-+-+-
+    //ERROR HANDLING
+    //+-+-+-+-+-+-+-+-+-
     catch (cl::BuildError& error) // If kernel failed to build
     {
         std::cerr << error.what() << "(" << error.err() << ")" << std::endl;
@@ -126,5 +151,49 @@ int main()
     {
         std::cerr << error.what() << std::endl;
         std::exit(EXIT_FAILURE);
+    }
+}
+//+-+-+-+-+-+-+-+-+-
+//Functions to write out and dump the current status
+//+-+-+-+-+-+-+-+-+-
+void writeGrid(std::vector<int> grid, unsigned int gen, time_t genTime, unsigned int N, unsigned int M)
+{
+    //seting up the string name for the file
+    std::stringstream fileName(""); fileName << "C:\\Users\\220mp\\Documents\\ELTE_MSc\\GPU\\Project\\GPU\\outputs\\" << "-"<< N << "x" << N << "-gen_" << gen  << ".txt";
+    std::ofstream file(fileName.str().c_str());
+    //header
+    file << "#T " << genTime << std::endl;
+    //body
+    unsigned int idx;
+    for (unsigned int i_row = 0; i_row < N; i_row++)
+    {
+        for (unsigned int i_col = 0; i_col < M; i_col++)
+        {
+            idx = i_row * M + i_col;
+            if (grid[idx] == 1)
+                file << "+,";
+            else
+                file << "-,";
+        }
+        file << "\n";
+    }
+    file.close();
+
+}
+void dumpGrid(std::vector<int> grid, unsigned int gen, time_t genTime, unsigned int N, unsigned int M, bool print) //dumps the status inserted to console
+{
+    unsigned int idx;
+    std::cout << "gen: " << gen << "\ttime in us: " << genTime << std::endl;
+    if(print)
+    {
+        for (unsigned int i_row = 0; i_row < N; i_row++)
+        {
+            for (unsigned int i_col = 0; i_col < M; i_col++)
+            {
+                idx = i_row * M + i_col;
+                std::cout << grid[idx];
+            }
+            std::cout << "\n";
+        }
     }
 }
--- GPU/funcs.cl
+++ GPU/funcs.cl
@@ -1,40 +1,39 @@
-__kernel void applyRules(double a, __global double* x, __global double* y)
+__kernel void applyRules(__global int* grid1, __global int* grid2, int N, int M, int rule)
 {
-    size_t gid = get_global_id(0);
-    y[gid] = a * x[gid] + y[gid];
-    /*
-    int temp;
-    int idx = 0;
+    //get the two dimension
+    int i_row = get_global_id(0);
+    int i_col = get_global_id(1);
+    //initialize
+    int neighbors = 0;
+    int x = i_row;
+    int y = i_col;
+    //solution for the edges
+    if(i_row-1 == -1) x += N;
+    if(i_col-1 == -1) y += M;
+    if(i_row == N) x -= N;
+    if(i_col == M) y -= M;
+    //index for the 1D array
+    int idx = x * M + y;
     //loop throught the grid, excluding the border indices
-    for (int i_row = 1; i_row < m_rows-1; i_row++)
+    neighbors += grid1[(x - 1) * M + y] + //up
+            grid1[(x + 1) * M + y] + //down
+            grid1[x * M + (y - 1)] + //left
+            grid1[x * M + (y + 1)] + //right
+            grid1[(x - 1) * M + (y - 1)] + //up and right
+            grid1[(x - 1) * M + (y + 1)] + //up and right
+            grid1[(x + 1) * M + (y - 1)] + //down and right
+            grid1[(x + 1) * M + (y + 1)]; //down and right
+    //Decision on the life of the cell.
+    if (neighbors == rule) //stays
     {
-        for (int i_col = 1; i_col < m_cols-1; i_col++)
-        {
-            temp = 0;                                                //holder for dead or alive cells
-            idx = i_row * m_cols + i_col;
-            temp += grids[0][(i_row - 1) * m_cols + i_col] + //up
-                    grids[0][(i_row + 1) * m_cols + i_col] + //down
-                    grids[0][i_row * m_cols + (i_col - 1)] + //left
-                    grids[0][i_row * m_cols + (i_col + 1)] + //right
-                    grids[0][(i_row - 1) * m_cols + (i_col - 1)] + //up and right
-                    grids[0][(i_row - 1) * m_cols + (i_col + 1)] + //up and right
-                    grids[0][(i_row + 1) * m_cols + (i_col - 1)] + //down and right
-                    grids[0][(i_row + 1) * m_cols + (i_col + 1)]; //down and right
-            
-            if (temp == m_rule) //stays
-            {
-                grids[1][idx] = grids[0][idx];
-            }
-            else if (temp == m_rule + 1) //lives
-            {
-                grids[1][idx] = 1;
-            }
-            else //dies
-            {
-                grids[1][idx] = 0;
-            }
-        }
+        grid2[idx] = 1*grid1[idx];
     }
-    grids[0] = grids[1];
-    */
+    else if (neighbors == rule + 1) //lives
+    {
+        grid2[idx] = 1;
+    }
+    else //dies
+    {
+        grid2[idx] = 0;
+    }
 }
